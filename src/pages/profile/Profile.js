76c5ffb22ec3c6d8d63d337e62c1a386c0345e1d
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:25:42 +0700

    Update Profile.js

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import ProfileSeller from "./ProfileSeller";
@@ -31,37 +30,3 @@ class Profile extends React.Component {
 }
 
 export default Profile;
-=======
-import React from "react";
-
-import ProfileSeller from "./ProfileSeller";
-import ProfileBuyer from "./ProfileBuyer";
-import Header from "../../layout/header/Header";
-
-import './Profile.css'
-
-class Profile extends React.Component {
-    renderContentByType() {
-        const { type } = this.props;
-
-        if (type === "seller") return <ProfileSeller />;
-        if (type === "buyer") return <ProfileBuyer />;
-        return <div>Неизвестный тип профиля</div>;
-    }
-
-    render() {
-        return (
-            <>
-            <Header type="profile" />
-            <main>
-                <div className="profile-panel">
-                    {this.renderContentByType()}
-                </div>                
-            </main>            
-            </>
-        );
-    }
-}
-
-export default Profile;
->>>>>>> 529fbc0 (New commit)
