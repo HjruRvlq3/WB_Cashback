cf2fec2c5ba149b169bb54f2ebe7eed70a1c4e90
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:13:18 +0700

    Update Button.js

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// Button.js
 import React from "react"
 import './Button.css'
 
@@ -21,27 +19,3 @@ class Button extends React.Component {
 }
 
 export default Button
-=======
-// Button.js
-import React from "react"
-import './Button.css'
-
-class Button extends React.Component {
-    render() {
-        const { icon: Icon, title, type = "button", ...rest } = this.props;
-
-        return (
-            <button 
-                className="default-button"
-                type={type}
-                {...rest} // Прокидывает onClick и другие пропсы
-            >
-                {Icon && <Icon className="profile-action__icon" />}
-                {title}
-            </button>
-        )
-    }
-}
-
-export default Button
->>>>>>> 529fbc0 (New commit)
