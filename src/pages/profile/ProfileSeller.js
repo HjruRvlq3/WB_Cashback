bf7b90b7e94a98cbdd1374f349a0f4d636d0a8e5
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:26:06 +0700

    Update ProfileSeller.js

--- src/pages/profile/ProfileSeller.js
+++ src/pages/profile/ProfileSeller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import { NavLink } from "react-router-dom"
 
@@ -32,38 +31,3 @@ class ProfileSeller extends React.Component {
 }
 
 export default ProfileSeller
-=======
-import React from "react"
-import { NavLink } from "react-router-dom"
-
-import ProfileAction from "../../components/profile-action/ProfileAction"
-
-import { ReactComponent as AddProduct } from '../../img/nav/add product.svg';
-import { ReactComponent as Support } from '../../img/support.svg';
-
-class ProfileSeller extends React.Component {
-    render() {
-        return (
-            <>
-            <NavLink to="/add-product" className="no-style-link">
-                <ProfileAction 
-                    icon={AddProduct}
-                    title="Разместить товар"
-                    subtitle="Для продавца"
-                />                
-            </NavLink>
-            
-            <div className="profile-divider" />
-
-            <ProfileAction 
-                icon={Support}
-                title="Поддержка"
-                subtitle="Оперативно ответим на все ваши вопросы"
-            />
-            </>
-        )
-    }
-}
-
-export default ProfileSeller
->>>>>>> 529fbc0 (New commit)
