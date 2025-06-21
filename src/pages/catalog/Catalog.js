104d3b216a40dcd354947811aa946dbe60f961d4
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:24:56 +0700

    Update Catalog.js

--- src/pages/catalog/Catalog.js
+++ src/pages/catalog/Catalog.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 
 import Header from "../../layout/header/HeaderTransparent"
@@ -14,20 +13,3 @@ class Catalog extends React.Component {
 }
 
 export default Catalog
-=======
-import React from "react"
-
-import Header from "../../layout/header/HeaderTransparent"
-
-class Catalog extends React.Component {
-    render() {
-        return (
-            <>
-            <Header title="Каталог" />
-            </>
-        )
-    }
-}
-
-export default Catalog
->>>>>>> 529fbc0 (New commit)
