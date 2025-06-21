c756510090b13d0f006106f5e517629a1077519f
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:22:44 +0700

    Update HeaderProfile.js

--- src/layout/header/HeaderProfile.js
+++ src/layout/header/HeaderProfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import './HeaderProfile.css';
 import { ReactComponent as ProfileIcon } from '../../img/nav/profile.svg';
@@ -36,42 +35,3 @@ class Header extends React.Component {
 }
 
 export default Header;
-=======
-import React from "react";
-import './HeaderProfile.css';
-import { ReactComponent as ProfileIcon } from '../../img/nav/profile.svg';
-
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userName: "Гость",
-        };
-    }
-
-    componentDidMount() {
-        const user = window.Telegram.WebApp.initDataUnsafe?.user;
-        if (user) {
-            const fullName = user.first_name + (user.last_name ? " " + user.last_name : "");
-            this.setState({ userName: fullName });
-        }
-    }
-
-    render() {
-        return (
-            <header className="header-profile">
-                <div className="header-profile__user">
-                    <ProfileIcon />
-                    <span>{this.state.userName}</span>
-                </div>
-                <div className="header-profile__actions">
-                    {/* кнопка инструкция */}
-                    {/* кнопка о сервисе */}
-                </div>
-            </header>
-        )
-    }
-}
-
-export default Header;
->>>>>>> 529fbc0 (New commit)
