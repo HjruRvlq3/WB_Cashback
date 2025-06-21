d599f468f5d3be26ffb6105bb25eb749fc634124
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:19:38 +0700

    Update CatalogItem.js

--- src/components/catalog item/CatalogItem.js
+++ src/components/catalog item/CatalogItem.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import './CatalogItem.css'
 
@@ -22,28 +21,3 @@ class CatalogItem extends React.Component {
 }
 
 export default CatalogItem
-=======
-import React from "react"
-import './CatalogItem.css'
-
-class CatalogItem extends React.Component {
-    render() {
-        return (
-            <>
-            <div>                
-                <div></div> {/* картинка */}
-                <div>
-                    <div></div> {/* заголовок */}
-                    <div>
-                        <div></div> {/* старая цена */}
-                        <div></div> {/* новая цена */}
-                    </div>
-                </div>
-            </div>
-            </>
-        )
-    }
-}
-
-export default CatalogItem
->>>>>>> 529fbc0 (New commit)
