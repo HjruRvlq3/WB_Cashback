c15cbc4262698adac464edc282000206e02f08e7
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:23:55 +0700

    Update HeaderTransparent.js

--- src/layout/header/HeaderTransparent.js
+++ src/layout/header/HeaderTransparent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 
 import './HeaderTransparent.css'
@@ -17,23 +16,3 @@ class Header extends React.Component {
 }
 
 export default Header
-=======
-import React from "react"
-
-import './HeaderTransparent.css'
-
-class Header extends React.Component {
-    render() {
-
-        const { title } = this.props;
-
-        return (
-            <header className="header-transparent">
-                <span>{title}</span>                
-            </header>
-        )
-    }
-}
-
-export default Header
->>>>>>> 529fbc0 (New commit)
