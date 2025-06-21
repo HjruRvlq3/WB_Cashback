66cd34cc4bbc0e0a76f45b77aedb2c69ccd6e28c
HjruRvlq3 <122445982+HjruRvlq3@users.noreply.github.com>
2025-06-22 02:21:14 +0700

    Update ProfileAction.js

--- src/components/profile-action/ProfileAction.js
+++ src/components/profile-action/ProfileAction.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import { ReactComponent as Arrow } from '../../img/arrow.svg';
@@ -31,37 +30,3 @@ class ProfileAction extends React.Component {
 }
 
 export default ProfileAction;
-=======
-import React from "react";
-
-import { ReactComponent as Arrow } from '../../img/arrow.svg';
-
-import './ProfileAction.css'
-
-class ProfileAction extends React.Component {
-    render() {
-        const { icon: Icon, title, subtitle } = this.props;
-
-        return (
-            <>
-            <div className="profile-action">
-                <div className="profile-action__content"> {/* Основной контент слева: иконка + текст */}
-                    <div className="profile-action__icon-wrapper"> {/* Справа картинка */}
-                        {Icon && <Icon className="profile-action__icon" />}
-                    </div>
-                    <div className="profile-action__text"> {/* Слева текст */}
-                        <div className="profile-action__title">{title}</div> {/* Заголовок */}
-                        <div className="profile-action__subtitle">{subtitle}</div> {/* Подзаголовок */}
-                    </div>
-                </div>
-                <div className="profile-action__arrow"> {/* Стрелка справа */}
-                    <Arrow />
-                </div>
-            </div>
-            </>
-        );
-    }
-}
-
-export default ProfileAction;
->>>>>>> 529fbc0 (New commit)
